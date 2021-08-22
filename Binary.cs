e14539a214eb65eff4744c01ca0f2e46bc419514
xandeep <ersandeepthapa@gmail.com>
2021-08-22 22:14:23 +1000

    Tree Traverse Implementation

--- Binary.cs
+++ Binary.cs
@@ -6,8 +6,9 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Binary Search Tree!");
-            BinarySearchtree tree = new BinarySearchtree();
+            Console.WriteLine("Welcome to the Binary Search Tree! \n");
+            BinarySearchTree tree = new BinarySearchTree();
+
             tree.Insert(100);
             tree.Insert(75);
             tree.Insert(85);
@@ -19,81 +20,19 @@ namespace DSA
             tree.Insert(110);
             tree.Insert(115);
             tree.Insert(95);
+            tree.Insert(104);
+            tree.Insert(106);
+            tree.Insert(103);
+            tree.Insert(109);
+
+            Console.WriteLine("Pre-Order Traverse : ");
+            tree.PreOrderTraverse(tree.root);
+
+            Console.WriteLine("\n\nPost-Order Traverse : ");
+            tree.PostOrderTraverse(tree.root);
+
+            Console.WriteLine("\n\nIn-Order Traverse : ");
+            tree.InOrderTraverse(tree.root);
         }
-    }
-
-    class BinarySearchtree
-    {
-        public class Node
-        {
-            public  int Data;
-            public Node LeftChild;
-            public Node RightChild;
-
-            public Node(int data)
-            {
-                Data = data;
-            }
-        }
-
-        public Node root;
-
-        public BinarySearchtree()
-        {
-            root = null;
-        }
-        public void Insert(int data)
-        {
-            Node NewNode = new Node(data);
-            if (root == null)
-            {
-                root = NewNode;
-            }
-            else
-            {
-                Node curent = root;
-                while (true)
-                {
-                    if (data < curent.Data)
-                    {
-                        // check if left node is empty 
-                        // if empty you can add value here
-                        // else keep going left.
-
-                        if(curent.LeftChild == null)
-                        {
-                            curent.LeftChild = NewNode;
-                            return;
-                        } else
-                        {
-                            curent = curent.LeftChild;
-                            continue;
-                        }
-                       
-                      
-                    }
-
-                    if ( data > curent.Data)
-                    {
-                        // check if right node is empty 
-                        // if empty you can add value here
-                        // else keep going right.
-
-                        if (curent.RightChild == null)
-                        {
-                            curent.RightChild = NewNode;
-                            return;
-                        }
-                        else
-                        {
-                            curent = curent.RightChild;
-                            continue;
-                        }
-                    }
-
-                }
-            }
-        }
-
     }
 }
---
+++ BinarySearchTree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    class BinarySearchTree
+    {
+        public class Node
+        {
+            public int Data;
+            public Node LeftChild;
+            public Node RightChild;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int data)
+        {
+            Node NewNode = new Node(data);
+            if (root == null)
+            {
+                root = NewNode;
+            }
+            else
+            {
+                Node curent = root;
+                while (true)
+                {
+                    if (data < curent.Data)
+                    {
+                        // check if left node is empty 
+                        // if empty you can add value here
+                        // else keep going left.
+
+                        if (curent.LeftChild == null)
+                        {
+                            curent.LeftChild = NewNode;
+                            return;
+                        }
+                        else
+                        {
+                            curent = curent.LeftChild;
+                            continue;
+                        }
+
+
+                    }
+
+                    if (data > curent.Data)
+                    {
+                        // check if right node is empty 
+                        // if empty you can add value here
+                        // else keep going right.
+
+                        if (curent.RightChild == null)
+                        {
+                            curent.RightChild = NewNode;
+                            return;
+                        }
+                        else
+                        {
+                            curent = curent.RightChild;
+                            continue;
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        public void PreOrderTraverse(Node node)
+        {
+            if (node != null)
+            {
+               
+                Console.Write(node.Data + " ");
+                PreOrderTraverse(node.LeftChild);
+                PreOrderTraverse(node.RightChild);
+            }
+
+        }
+
+        public void PostOrderTraverse(Node node)
+        {
+            if (node != null)
+            {
+                PostOrderTraverse(node.LeftChild);
+                PostOrderTraverse(node.RightChild);
+                Console.Write(node.Data + " ");
+            }
+
+        }
+
+        public void InOrderTraverse(Node node)
+        {
+            if( node != null)
+            {
+                InOrderTraverse(node.LeftChild);
+                Console.Write(node.Data + " ");
+                InOrderTraverse(node.RightChild);
+            }
+
+        }
+
+    }
+}
